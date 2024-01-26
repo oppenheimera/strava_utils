065e503def87674664a97a6f7ad26821635b7c04
Ari Oppenheimer <oppenheimera@users.noreply.github.com>
2024-01-26 13:35:44 -0800

    Update heatmap.py for new Strava heatmap site

--- heatmap.py
+++ heatmap.py
@@ -2,12 +2,14 @@
 Utility for retrieving Strava Heatmap Tiles for use in Gaia custom layers
 """
 
-ride_heatmap_url = "https://www.strava.com/heatmap#13.73/-122.38767/48.62712/hot/ride"
-ski_heatmap_url = "https://www.strava.com/heatmap#13.73/-122.38767/48.62712/blue/winter"
+ride_heatmap_url = "https://www.strava.com/maps/global-heatmap?style=standard&terrain=false&sport=ride&gColor=hot&gOpacity=100#12.3/48.70698/-122.40974"
+ski_heatmap_url = "https://www.strava.com/maps/global-heatmap?style=standard&terrain=false&sport=winter&gColor=blue&gOpacity=100#13.05/48.8305/-121.63693"
 
 def format_url(url):
-	"""Takes in a heatmap URL with no TMS prefix and returns bike and ski URLs formatted for Gaia"""
+	"""Takes in a heatmap URL and returns bike and ski URLs formatted for Gaia"""
 	bike = url
+	if bike[:4] == "tms:": # Check for TMS prefix
+		bike = bike[4:]
 	bike = bike.replace("{switch:a,b,c}", "a")
 	bike = bike.replace("{zoom}", "{z}")
 	ski = bike.replace("ride", "winter")
@@ -27,4 +29,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-	pass
